7c0860fb9544faa01439e385ba4a8e38940b8859
mantour <mantour.tw@gmail.com>
2010-04-20 21:00:31 +0800

    106.cpp complete

--- excercise/biginteger.cpp
+++ excercise/biginteger.cpp
@@ -1,7 +1,8 @@
 /* 
- * implementation of biginteger algebra
+ * UVA 106
  */
 #include<iostream>
+#include<cstdio>
 #include<climits>
 #include<sstream>
 #include<string>
@@ -10,7 +11,7 @@
 using namespace std;
 int int_byte(){
     int i=1;
-    unsigned int test=UINT_MAX;
+    unsigned long test=ULONG_MAX;
     test/=2;
     while(test){
         i++;
@@ -18,11 +19,11 @@ int int_byte(){
     }
     return i;
 }
-const unsigned int base=1<<int_byte()/2;
-const int precision=8;
+const unsigned long base=1<<int_byte()/2;
+const int precision=3;
 class BigInteger{
     public:
-    unsigned int num[precision];
+    unsigned long num[precision];
     BigInteger(){
         int i;
         for (i = 0; i < precision; i++) {
@@ -30,8 +31,9 @@ class BigInteger{
         }
         return;
     }
-    BigInteger(unsigned n){
-        int i=1,q=n/base,r=n%base;
+    BigInteger(unsigned long n){
+        int i=1;
+        unsigned long q=n/base,r=n%base;
         this->num[0]=n%base;
         while(q!=0)
         {
@@ -53,17 +55,18 @@ class BigInteger{
     }
     friend BigInteger operator+(const BigInteger a,const BigInteger b);
     friend BigInteger operator-(const BigInteger a,const BigInteger b);
-    friend BigInteger mul(const BigInteger a,const unsigned int b,const unsigned int offset);
-    friend pair<BigInteger,unsigned int> div(const BigInteger a,const unsigned int b);
+    friend BigInteger mul(const BigInteger a,const unsigned long b,const unsigned long offset);
+    friend pair<BigInteger,unsigned long> div(const BigInteger a,const unsigned long b);
     friend BigInteger operator*(const BigInteger a,const BigInteger b);
     static int compare(const BigInteger a,const BigInteger b);
-    string str();
+    string toString();
+    unsigned long toULong();
 };
 
 BigInteger operator+(const BigInteger a,const BigInteger b){
     int i;
-    unsigned int sum;
-    unsigned int carry=0;
+    unsigned long sum;
+    unsigned long carry=0;
     BigInteger result(0);
     for (i = 0; i < precision; i++) {
         sum=a.num[i]+b.num[i]+carry;
@@ -75,8 +78,8 @@ BigInteger operator+(const BigInteger a,const BigInteger b){
 
 BigInteger operator-(const BigInteger a,const BigInteger b){
     int i;
-    unsigned int dif;
-    unsigned int carry=0;
+    unsigned long dif;
+    unsigned long carry=0;
     BigInteger result(0);
     for (i = 0; i < precision; i++) {
         if(a.num[i] >= b.num[i] + carry){
@@ -90,10 +93,10 @@ BigInteger operator-(const BigInteger a,const BigInteger b){
     return result;
 }
 
-BigInteger mul(const BigInteger a,const unsigned int b,const unsigned int offset=0){
+BigInteger mul(const BigInteger a,const unsigned long b,const unsigned long offset=0){
     int i;
-    unsigned int pro;
-    unsigned int carry=0;
+    unsigned long pro;
+    unsigned long carry=0;
     BigInteger result(0);
     for (i = 0; i < precision-offset; i++) {
         pro=b*a.num[i];
@@ -103,17 +106,17 @@ BigInteger mul(const BigInteger a,const unsigned int b,const unsigned int offset
     return result;
 }
 
-pair<BigInteger,unsigned int> div(const BigInteger a,const unsigned int b){
+pair<BigInteger,unsigned long> div(const BigInteger a,const unsigned long b){
     int i;
-    unsigned int dividend;
-    unsigned int remander=0;
+    unsigned long dividend;
+    unsigned long remander=0;
     BigInteger result(0);
     for (i = precision-1; i >= 0 ; i--) {
         dividend=a.num[i]+(base)*remander;
         result.num[i]=dividend/b;
         remander=dividend%b; 
     }
-    return pair<BigInteger,unsigned int>(result,remander);
+    return pair<BigInteger,unsigned long>(result,remander);
 }
 
 int BigInteger::compare(BigInteger a,BigInteger b){
@@ -128,10 +131,10 @@ int BigInteger::compare(BigInteger a,BigInteger b){
     return 0;
 }
 
-string BigInteger::str(){
+string BigInteger::toString(){
     stringstream result;
     string strresult;
-    pair<BigInteger,unsigned int> QnR(BigInteger(),0);
+    pair<BigInteger,unsigned long> QnR(BigInteger(),0);
     QnR.first=*this;
     while(compare(QnR.first,BigInteger(0)))
     {
@@ -143,8 +146,12 @@ string BigInteger::str(){
     return strresult;
 }
 
+unsigned long BigInteger::toULong(){
+    return num[0]+num[1]*base;
+}
+
 BigInteger operator*(const BigInteger a,const BigInteger b){
-    unsigned int i;
+    int i;
     BigInteger result(0);
     for (i = 0; i < precision; i++) {
         result=result+mul(a,b.num[i],i);
@@ -152,9 +159,82 @@ BigInteger operator*(const BigInteger a,const BigInteger b){
     return result; 
 }
 
+BigInteger bigMul(unsigned long a, unsigned long b){
+    BigInteger A(a),B(b);
+    return A*B;
+}
+
+unsigned long gcd(unsigned long a, unsigned long b){
+    if(a<b){
+        return gcd(b,a);
+    }
+    else if(a%b==0){
+        return b;
+    }
+    else{
+        return gcd(b,a%b);
+    }
+}
+
+unsigned long bgcd(unsigned long u, unsigned long v){
+     int shift;
+     /* GCD(0,x) := x */
+     if (u == 0 || v == 0)
+       return u | v;
+     /* Let shift := lg K, where K is the greatest power of 2
+        dividing both u and v. */
+     for (shift = 0; ((u | v) & 1) == 0; ++shift) {
+         u >>= 1;
+         v >>= 1;
+     }
+     while ((u & 1) == 0)
+       u >>= 1;
+     /* From here on, u is always odd. */
+     do {
+         while ((v & 1) == 0)  /* Loop X */
+           v >>= 1;
+ 
+         /* Now u and v are both odd, so diff(u, v) is even.
+            Let u = min(u, v), v = diff(u, v)/2. */
+         if (u < v) {
+             v -= u;
+         } else {
+             unsigned long diff = u - v;
+             u = v;
+             v = diff;
+         }
+         v >>= 1;
+     } while (v != 0); 
+    return u << shift;
+}
+
+
+unsigned long isSquare(BigInteger a){
+    unsigned long x=1;
+    for(;;){
+        if(BigInteger::compare(bigMul(x,x),a)==0){
+            return x;
+        }
+        if(BigInteger::compare(bigMul(x,x),a)==1){
+            return 0;
+        }
+        x++;
+    }
+}
+
+unsigned long isSquare(unsigned long a){
+    unsigned long x=1;
+    for(;;x++){
+        if(x*x==a){
+            return x;
+        }
+        if(x*x>a){
+            return 0;
+        }
+    }
+}
+
 int main(){
-    BigInteger a(1000000);
-    BigInteger b(500000);
-    cout<<(a-b).str()<<endl;
+    cout<<bigMul(1000000,1000000).toString()<<endl;
     return 0;
 }
