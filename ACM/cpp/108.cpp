14918bedda2a15349359eeef7b5038b198a5c69a
mantour <mantour.tw@gmail.com>
2010-04-24 00:20:28 +0800

    reorganized the codes

--- ACM/cpp/108.cpp
+++ ACM/cpp/108.cpp
@@ -35,8 +35,41 @@ void printMatrix(int N,int **A){
     }
 }
 
+int columnSum(int j,int a,int b,int **A){
+    /*
+     *   return the sum of A[a][j] to A[b][j]
+     */
+    int sum=0;
+    for(int i=a;i<=b;i++){
+        sum+=A[i][j];
+    }
+    return sum;
+}
+
+int extend(int N,int j,int **A,int **B){
+    /*
+     *  B[a][b] is the max sum of boxes from roa a to row b, with right edge at column j
+     *  this function extends it to max sum of boxes with right edge at column j+1, and then
+     *  return the maximum of B
+     */
+    int max;
+    for(int a=0;a<N;a++){
+        for(int b=a;b<N;b++){
+            if(B[a][b]<0){
+                B[a][b]=0;
+            }
+            B[a][b]+=columnSum(j,a,b,A);
+            if((a==0&&b==0)||B[a][b]>max){
+                max=B[a][b];
+            }
+        }
+    }
+    return max;
+    //printMatrix(N,B);
+}
+
 int maxSum(int N,int **A){
-    int maxOfAll=0;
+    int maxOfAll,max;
     int ** B; 
         //max sum of for right sided boxes from row a to row b
     constructMatrix(N,&B);
@@ -44,23 +77,13 @@ int maxSum(int N,int **A){
         for(int b=0;b<N;b++){
             B[a][b]=0;
         }
-    }
+    } // initialize B
     for(int j=0;j<N;j++){
-        for(int a=0;a<N;a++){
-            for(int b=a;b<N;b++){
-                if(B[a][b]<0){
-                    B[a][b]=0;
-                }
-                for(int i=a;i<=b;i++){
-                    B[a][b]+=A[i][j];
-                }
-                if(B[a][b]>maxOfAll){
-                    maxOfAll=B[a][b];
-                }
-            }
+        max=extend(N,j,A,B);
+        if(j==0||max>maxOfAll){
+            maxOfAll=max;
         }
-        //printMatrix(N,B);
-    }
+    } // extend right edge of boxes from j=0 to j=N-1, record the maxSum ever meet
     destructMatrix(N,B);
     return maxOfAll;
 }
